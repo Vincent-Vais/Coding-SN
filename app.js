0d8068c4156f538c11004cd096fcc5247d89e625
Vincent Vais <vincentvais23@gmail.com>
2020-07-22 23:53:37 -0500

    change blog page

--- app.js
+++ app.js
@@ -104,33 +104,53 @@ app.get("/", function(req, res){
 	res.redirect("/blogs");
 })
 
+// ######################## BLOG PAGE #########################
+
 app.get("/blogs", function(req, res){
 	Blog.find({}, function(err, blogs){
 		if(err){
 			console.log("Error! ", err);
 		}else{
-			res.render("index", {blogs: blogs});
+			for (let blog of blogs){
+				User.find({'myBlogs' : mongoose.Types.ObjectId(`${blog._id}`)}, (err, foundUser) => {
+					if(err){
+						console.log(err);
+					}else{
+						blog.author = foundUser[0].username;
+						blog.save();
+					}
+				});
+			}
+			res.render("blogs", {blogs: blogs});
 		}
 	});
 });
 
-app.get("/blogs/new", function(req, res){
+app.get("/blogs/new", isLoggedIn, function(req, res){
 	res.render("new");
 });
 
 app.post("/blogs", function(req, res){
 	req.body.blog.body = req.sanitize(req.body.blog.body);
+	console.log(req.user);
 	Blog.create(req.body.blog, function(err, newBlog){
 		if(err){
 			console.log("Error! ", err);
-			res.render("new");
 		}else{
-			res.redirect("/blogs");
+			User.findById(req.user._id, (err, foundUser) => {
+				if(err){
+					console.log(err);
+				}else{
+					foundUser.myBlogs.push(newBlog);
+					foundUser.save();
+					res.redirect("/blogs");
+				}
+			});
 		}
 	});
 });
 
-app.get("/blogs/:id", function(req, res){
+app.get("/blogs/:id", isLoggedIn, function(req, res){
 	Blog.findById(req.params.id, function(err, foundBlog){
 		if(err){
 			console.log("Error! ", err);
@@ -141,7 +161,7 @@ app.get("/blogs/:id", function(req, res){
 	});
 });
 
-app.get("/blogs/:id/edit", function(req, res){
+app.get("/blogs/:id/edit", isLoggedIn, function(req, res){
 	Blog.findById(req.params.id, function(err, foundBlog){
 		if(err){
 			console.log("Error! ", err);
@@ -153,7 +173,7 @@ app.get("/blogs/:id/edit", function(req, res){
 	
 })
 
-app.put("/blogs/:id", function(req, res){
+app.put("/blogs/:id", isLoggedIn, function(req, res){
 	req.body.blog.body = req.sanitize(req.body.blog.body);
 	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
 		if(err){
@@ -165,7 +185,7 @@ app.put("/blogs/:id", function(req, res){
 	});
 });
 
-app.delete("/blogs/:id", function(req, res){
+app.delete("/blogs/:id", isLoggedIn, function(req, res){
 	Blog.findByIdAndRemove(req.params.id, function(err){
 		if(err){
 			console.log("Error! ", err);
@@ -176,6 +196,7 @@ app.delete("/blogs/:id", function(req, res){
 	});
 });
 
+// ######################## USER PAGE #########################
 app.get("/mypage/:id", isLoggedIn, (req, res) => {
 	res.render("showUser");
 });
@@ -234,7 +255,7 @@ app.put("/mypage/:id", isLoggedIn, (req, res) => {
 });
 
 
-// LOGIN GET
+// ######################## AUTHENTICATION #########################
 app.get("/login", (req, res) => {
 	console.log("GET TO LOGIN");
 	res.render("login");
@@ -280,8 +301,3 @@ app.get("/logout", (req, res) => {
 app.listen(process.env.PORT || 3000, () => {
 	console.log("server has started");
 });
-
-
-// const ifLoggedIn = (req, res, next){
-
-// }
