5454a32a6fad824b10c04d6e2b178e02dd06edf0
Vincent Vais <vincentvais23@gmail.com>
2020-07-24 20:45:55 -0500

    add some animation for blogs

--- public/js/blogs.js
+++ public/js/blogs.js
@@ -1,2 +1,45 @@
 $('.ui.dropdown')
   .dropdown();
+
+$(".image-container img").mouseover(function(e){
+  if(this === e.target){
+    $( ".box .aside-container" ).animate({
+      "flex-basis": "100%"
+    }, 1000, function(){
+      $(".content-container").css("display","flex");
+    });
+  }
+});
+
+const element = document.querySelector(".content-item");
+
+const box = (function(elm){
+  const elems = [];
+  function makeTree(node){
+    elems.push(node);
+    if(node.children){
+      let arr = Array.from(node.children);
+      for (let child of arr){
+        makeTree(child);
+      }
+    }
+  }
+  makeTree(elm);
+  return elems;
+})(element);
+
+for (let item of box){
+  item.addEventListener("mouseout", outHandler);
+}
+
+function outHandler(e){
+  let elementMouseIsOver = document.elementFromPoint(event.clientX, event.clientY);
+  console.log(elementMouseIsOver);
+  let inside = box.findIndex(item => item === elementMouseIsOver);
+  console.log(inside);
+  if(inside === -1){
+    $(".content-container").css("display","none");
+    $( ".box .aside-container" ).animate({
+      "flex-basis": "10%"}, 1000);
+  }
+}
